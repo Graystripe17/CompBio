d6cd5e7bec8804c953912b547173373d8de54f46
Winston Van <winston.van7@gmail.com>
2020-02-07 16:44:05 -0600

    Gives decent alignments.

--- Project1/NW.py
+++ Project1/NW.py
@@ -16,85 +16,43 @@ def loadBlosum(filename="blosum.txt"):
     assert(len(weightsDict) == 300)
     return weightsDict
 
-weightsDict = loadBlosum()
-
 def loadSequence(filename):
     with open(filename, "r+") as oh:
         first = oh.readline()[1:].strip()
         seq   = map(str.strip, oh.readlines())
     return ''.join(seq)
 
-seq1 = loadSequence("1dzl_HPV.fasta")
-seq2 = loadSequence("3mge_HIV.fasta")
-
-import numpy as np
-import seaborn as sns; sns.set()
 def printMatrix(grid):
+    #   seq 1
+    # s
+    # e
+    # q
+    # 2
     print('\n'.join([''.join(['{:4}'.format(item) for item in row])
       for row in grid]))
-    ax = sns.heatmap(np.array(grid))
-
-
-#seq1 = "EEEEEKKKKK"
-#seq2 = "EEEEE"
-
-#seq1 = "ABCDEFK"
-#seq2 = "ABCDEKK"
-
-#seq1 = "AAAAAFFF"
-#seq2 = "BBBBBFFF"
-
-#seq1 = "GTACAXGCAAVVVVVVVVVAAAAAAA"
-#seq2 = "GTACCAXGCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" # THIS BREAKS
-
-seq1 = "ARN"
-seq2 = "ARN"
-
-#   seq 1
-# s
-# e
-# q
-# 2
-
-# Build empty grid
-grid = [[0 for i in range(len(seq1) + 1)] for j in range(len(seq2) + 1)]
 
 def getBestScore(i, j):
     # Get diag neighbor
     if j > 0 and i > 0:
         row = i - 1
         col = j - 1
-        assert(row >=0 and col >= 0)
-        c = weightsDict[alphabetize(seq1[col-1], seq2[row-1])]
-        print("diag", seq1[col-1], seq2[row-1])
-        if i == 1 and j == 1:
-            print("YEHAW", seq1[col-1], seq2[row-1])
-            print(c, grid[row][col])
+        c = weightsDict[alphabetize(seq1[col], seq2[row])]
         d_score = grid[row][col] + c
     # Get left neighbor
     if j > 0:
         row = i
         col = j - 1
-        assert(row >=0 and col >= 0)
         # Gapping seq1
-        c = weightsDict[alphabetize('*', seq2[row-1])]
-        print("left", seq2[row-1])
+        c = weightsDict[alphabetize('*', seq1[col])]
         l_score = grid[row][col] + c
     # Get up neighbor
     if i > 0:
         row = i - 1
         col = j
-        assert(row >=0 and col >= 0)
         # Gapping seq2
-        c = weightsDict[alphabetize(seq1[col-1], '*')]
-        print("up", seq1[col-1])
+        c = weightsDict[alphabetize(seq2[row], '*')]
         u_score = grid[row][col] + c
-    print("")
-    if i == 1 and j == 1:
-        print("SHOULD BE 4:", d_score, l_score, u_score)
-        print(seq1[col-1], seq2[row-1])
     return max(d_score, l_score, u_score)
-
 
 def maxNeedlemanWunsch():
     # Populate grid with gaps
@@ -124,13 +82,6 @@ def maxSmithWaterman():
                 maxScore = i, j, bestScore 
     return maxScore
 
-maxScore = maxNeedlemanWunsch()
-
-printMatrix(grid)
-print(maxScore[2])
-
-backtrackSteps = []
-
 def backtrack(i, j):
     if i == 0 and j == 0:
         return
@@ -157,9 +108,7 @@ def backtrack(i, j):
         u_neighbor = grid[row][col]
     # As long as it's the largest, push onto the stack
     # Invariant: max backwards always converges
-    print(i, j, backtrackSteps)
     bestNeighbor = max(d_neighbor, l_neighbor, u_neighbor)
-    print("BDLU", bestNeighbor, d_neighbor, l_neighbor, u_neighbor)
     if bestNeighbor == d_neighbor:
         backtrackSteps.append('D')
         backtrack(i - 1, j - 1)
@@ -172,66 +121,106 @@ def backtrack(i, j):
         backtrackSteps.append('2')
         backtrack(i - 1, j)
 
-backtrack(maxScore[0], maxScore[1])
-#backtrack(len(seq2), len(seq1))
-print("BTSTEPS", backtrackSteps)
-steps = backtrackSteps[::-1]
-print("STEPS", steps)
-
 def seqGenerator(seq):
     i = 0
     while i < len(seq):
         yield seq[i]
         i += 1
 
-seq1Generator = seqGenerator(seq1)
-seq2Generator = seqGenerator(seq2)
+def wordWrapGenerator(seq):
+    i = 80
+    while i < len(midList):
+        yield seq1[i-80:i]
+        i += 80
 
-# Printing line 1
-seq1List = []
-midList  = []
-seq2List = []
-for s in steps:
-    if s == 'D':
-        f = next(seq1Generator)
-        n = next(seq2Generator)
-        print(f, n)
-        seq1List.append(f)
-        if f == n:
-            midList.append('|')
-        else:
-            midList.append('*')
-        seq2List.append(n)
-    elif s == '1': # THIS USED TO SAY 1
-        seq1List.append(next(seq1Generator))
-        midList.append(' ')
-        seq2List.append('-')
-    elif s == '2':
-        seq1List.append('-')
-        midList.append(' ')
-        seq2List.append(next(seq2Generator))
-# The max has been reached
-try:
-    while True:
-        seq1List.append(next(seq1Generator))
-        midList.append(' ')
-        seq2List.append('-')
-except:
-    pass
-try:
-    while True:
-        seq2List.append(next(seq2Generator))
-        seq1List.append('-')
-        midList.append(' ')
-except:
-    pass
-print(seq1List, midList, seq2List)
-print(''.join(seq1List))
-print(''.join(midList))
-print(''.join(seq2List))
-with open("alignment.txt", "w") as oh:
-    oh.write(''.join(seq1List))
-    oh.write('\n')
-    oh.write(''.join(midList))
-    oh.write('\n')
-    oh.write(''.join(seq2List))
+def printWrapped(seq1, midList, seq2, n=80):
+    i = n
+    while i < len(midList):
+        print(seq1[i-n:i])
+        print(midList[i-n:i])
+        print(seq2[i-n:i], end="\n\n\n")
+        i += n
+
+if __name__ == "__main__":
+    print("Enter (N)eedleman-Wunsch or (S)mith-Waterman")
+    algo = input().strip()
+    print("Enter \n(1) HPV vs HIV \n(2) Dengue vs Zika\n(3) Polio and rhino\n(4) Other")
+    sel = input().strip()
+    if sel == '1':
+        seq1 = loadSequence("1dzl_HPV.fasta")
+        seq2 = loadSequence("3mge_HIV.fasta")
+    elif sel == '2':
+        seq1 = loadSequence("1k4rA_dengue_virus.fasta")
+        seq2 = loadSequence("5ire_zika_virus.fasta")
+    elif sel == '3':
+        seq1 = loadSequence("2plv1_polio_virus.fasta")
+        seq2 = loadSequence("4rhv1_rhino_virus.fasta")
+    else:
+        print("Enter seq1")
+        seq1 = input()
+        print("Enter seq2")
+        seq2 = input()
+
+    weightsDict = loadBlosum()
+
+    # Build empty grid
+    grid = [[0 for i in range(len(seq1) + 1)] for j in range(len(seq2) + 1)]
+
+    if algo == 'N':
+        maxScore = maxNeedlemanWunsch()
+    elif algo == 'S':
+        maxScore = maxSmithWaterman()
+
+    backtrackSteps = []
+    backtrack(maxScore[0], maxScore[1])
+    steps = backtrackSteps[::-1]
+
+    seq1Generator = seqGenerator(seq1)
+    seq2Generator = seqGenerator(seq2)
+
+    # Uncomment to debug matrix
+    printMatrix(grid)
+
+    # Printing line 1
+    seq1List = []
+    midList  = []
+    seq2List = []
+    for s in steps:
+        if s == 'D':
+            f = next(seq1Generator)
+            n = next(seq2Generator)
+            seq1List.append(f)
+            if weightsDict[alphabetize(f, n)] > 0:
+                midList.append('|')
+            else:
+                midList.append('*')
+            seq2List.append(n)
+        elif s == '1': # THIS USED TO SAY 1
+            seq1List.append(next(seq1Generator))
+            midList.append(' ')
+            seq2List.append('-')
+        elif s == '2':
+            seq1List.append('-')
+            midList.append(' ')
+            seq2List.append(next(seq2Generator))
+    # The max has been reached
+    try:
+        while True:
+            seq1List.append(next(seq1Generator))
+            midList.append(' ')
+            seq2List.append('-')
+    except:
+        pass
+    try:
+        while True:
+            seq2List.append(next(seq2Generator))
+            seq1List.append('-')
+            midList.append(' ')
+    except:
+        pass
+    printWrapped(''.join(seq1List), ''.join(midList), ''.join(seq2List), 80)
+    with open("alignment.txt", "w") as oh:
+        oh.write(''.join(seq1List) + '\n')
+        oh.write(''.join(midList) + '\n')
+        oh.write(''.join(seq2List))
+
